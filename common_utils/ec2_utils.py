e22654fda7c3ded49989b9987190774cf82e429b
Otto Valladares <ottov@upenn.edu>
2018-04-23 15:46:40 -0400

    Second commit

---
+++ common_utils/ec2_utils.py
@@ -0,0 +1,164 @@
+ #!/usr/bin/env python
+from __future__ import print_function
+
+import boto3
+import time
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+ec2client = boto3.client('ec2',region_name='us-east-1')
+
+TAG_NAME  = 'PROJECT'
+TAG_VALUE = 'AWS-BATCH'
+VOLUME_TYPE = 'gp2'
+
+def getEC2_Zone():
+    s = requests.Session()
+    retries = Retry(total=5,
+                backoff_factor=3,
+                status_forcelist=[ 500, 502, 503, 504 ])
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+
+    r = s.get('http://169.254.169.254/latest/meta-data/placement/availability-zone', timeout=30)
+    if not r.text:
+        return None
+    if r.status_code == 504:
+        return None
+    return r.text
+
+def getEC2_InstanceId():
+    s = requests.Session()
+    retries = Retry(total=5,
+                backoff_factor=3,
+                status_forcelist=[ 500, 502, 503, 504 ])
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+
+    r = s.get('http://169.254.169.254/latest/meta-data/instance-id', timeout=30)
+    if not r.text:
+        return None
+    if r.status_code == 504:
+        return None
+    return r.text
+
+
+def createEBS(sz=42):
+    """
+    Create a new EBS and attach to local host
+    :param sz: requested size in GB
+    :return: volume_id
+    """
+    az = getEC2_Zone()
+
+    if sz > 16384: sz = 16384
+    if sz < 1: sz = 1
+    res = ec2client.create_volume(
+            AvailabilityZone = az,
+            Encrypted = False,
+            VolumeType = VOLUME_TYPE,
+            Size = sz) # In GB, max 16384 for gp2
+
+    ec2client.create_tags(
+        Resources=[
+            res['VolumeId'],
+        ],
+        Tags=[
+            {
+                'Key': TAG_NAME,
+                'Value': TAG_VALUE
+            },
+        ]
+    )
+
+    return res['VolumeId']
+
+def getEBS_volId(devName):
+    iid = getEC2_InstanceId()
+    res = ec2client.describe_volumes(
+             Filters=[
+                  {'Name': 'attachment.instance-id', 'Values': [iid]},
+                  {'Name': 'attachment.device', 'Values': [devName]},
+             ],
+          )
+    if len(res['Volumes'])>0:
+       return res['Volumes'][0]['VolumeId']
+
+    return None
+
+def attachEBS(devName, vol):
+    iid = getEC2_InstanceId()
+
+    res = ec2client.describe_volumes(
+            VolumeIds=[ vol ]
+            )
+
+    # Check ready Volume
+    while res['Volumes'][0]['State'] != 'available':
+        time.sleep(1)
+        res = ec2client.describe_volumes(
+            VolumeIds=[ vol ]
+            )
+
+
+    res = ec2client.attach_volume(
+            InstanceId = iid,
+            VolumeId = vol,
+            Device = devName,
+        )
+    time.sleep(1)
+
+    res = ec2client.describe_volumes(
+            VolumeIds=[ vol ]
+            )
+
+    # wait until attached
+    while res['Volumes'][0]['Attachments'][0]['State'] != 'attached':
+        time.sleep(1)
+        res = ec2client.describe_volumes(
+            VolumeIds=[ vol ]
+            )
+
+    ec2client.modify_instance_attribute(
+        InstanceId = iid,
+        BlockDeviceMappings=[
+            {
+                'DeviceName': devName,
+                'Ebs': {
+                    'VolumeId': vol,
+                    'DeleteOnTermination': True,
+                }
+            },
+        ]
+    )
+
+
+    if res['Volumes'][0]['Attachments'][0]['State'] == 'attached':
+        return 1
+
+    return 0
+
+def detachEBS(devName, vol):
+    print ("Detaching " + devName)
+    iid = getEC2_InstanceId()
+    ec2client.detach_volume(
+        Device = devName,
+        Force = False,
+        InstanceId=iid,
+        VolumeId=vol
+    )
+
+    res = ec2client.describe_volumes(
+            VolumeIds=[ vol ]
+            )
+
+    # Check ready Volume
+    while res['Volumes'][0]['State'] != 'available':
+        time.sleep(1)
+        res = ec2client.describe_volumes(
+            VolumeIds=[ vol ]
+            )
+
+def deleteEBS(vol):
+    print("Deleting " + vol)
+    ec2client.delete_volume(
+        VolumeId = vol
+    )
---
+++ run-docker-ebs-manager.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import sys
+import time
+
+import docker
+import requests
+from subprocess import check_output
+from string import ascii_lowercase
+
+from ec2_utils import *
+
+dClient = docker.from_env()
+
+# Global container list
+containerMap = dict()
+
+def generateDeviceName():
+    """
+    Looks for available device names from /dev/xvdcz ... /dev/xvdaa
+    """
+    for d1 in reversed(ascii_lowercase[0:3]):
+        for d2 in reversed(ascii_lowercase):
+            try:
+                devName = '/dev/xvd%s%s' % (d1,d2)
+                f = os.stat(devName)
+            except:
+                return devName
+
+
+def buildInventory():
+    """
+    Modifies global container dict to track any new containers. 
+    Helps detect finished tasks
+    """
+    for container in dClient.containers.list():
+        if container.name == 'ecs-agent': continue
+
+        if not container.id in containerMap:
+            containerMap[container.id] = None
+            cOut = container.exec_run(cmd=['sh','-c', 'df -h /scratch | tail -1 | cut -d" " -f1']).output
+            devName = str.rstrip(cOut)
+            containerMap[container.id] = { 'devname': devName,
+                                           'vol'    : getEBS_volId(devName)
+                                         }
+
+
+def dropFromInventory(cId):
+    """
+    Removes container from global tracking dict
+    """
+    if cId in containerMap:
+      print ('Removing container %s from inventory' % (cId))
+      devName = containerMap[cId]['devname']
+      vol     = containerMap[cId]['vol']
+      detachEBS(devName, vol)
+      deleteEBS(vol)
+      del containerMap[cId]
+    else:
+     print ('Continer %s, not found' %(cId))
+
+def mountEBS_on_container(devName, cId):
+    """
+    Executes commands on the given privileged container to mount assigned device
+    :param devName: block device (e.g. /dev/xvdcz)
+    :param cId: container id
+    """
+    cPath = '/scratch'
+    blkStr = check_output('lsblk --noheadings --output MAJ:MIN %s' % (devName), shell=True)
+
+
+    container = dClient.containers.get(cId)
+
+    container.exec_run(cmd='mknod %s b %s %s ' % (devName, blkStr.split(':')[0], str.rstrip(blkStr.split(':')[1])) )
+    container.exec_run(cmd='mkdir -p %s' % (cPath))
+    container.exec_run(cmd='mount %s %s' % (devName, cPath))
+
+
+def main():
+    """
+    Loops, waiting for containers to appear. 
+    Once they are running, checks to see if that have /TOTAL_SIZE request.
+    Makes an EBS drive from that request, and mounts it on corresponding container.
+    """
+
+    while True:
+        buildInventory()
+
+        for cItem in list(containerMap):
+
+            try:
+               c = dClient.containers.get(cItem)
+
+               print('Examining %s' % c.name)
+               cmd = c.exec_run('cat /TOTAL_SIZE')
+               if cmd.exit_code != 0: continue
+
+               sz = str.rstrip(cmd.output) # expecting bytes
+               volSz = int(sz) / 1024**3   # converted to GB for EBS sizing
+               volSz += 1                  # +1 !
+
+               # check if /scratch already mounted
+               cMounts = c.exec_run('cat /proc/mounts').output
+
+               if 'scratch' in cMounts: continue # already mounted
+
+               print('Creating, attaching EBS %s GB' % volSz)
+               vol = createEBS(volSz)
+               devName = generateDeviceName()
+               attachEBS(devName, vol)
+
+               print('mounting %s' % devName)
+               check_output('sudo mkfs.ext4 %s' % (devName), shell=True)
+
+               mountEBS_on_container(devName, c.short_id)
+               containerMap[cItem] = { 'devname': devName, 'vol': vol }
+               time.sleep(3)
+
+            except (docker.errors.NotFound, docker.errors.APIError):
+               print("docker not found. must delete.")
+               dropFromInventory(cItem)
+               continue
+
+        #print('No work. Sleeping')
+        try:
+          time.sleep(10)
+        except KeyboardInterrupt:
+          pass
+        except:
+          print("Unexpected error:", sys.exc_info()[0])
+          raise
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
